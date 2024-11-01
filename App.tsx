817196a847127da19fb107ab07a3d9c2bd61399f
Quốc Trưởng KT <truong.dinh@hoc10.com>
2024-11-01 15:55:38 +0700

    integration unistyles

--- App.tsx
+++ App.tsx
@@ -8,17 +8,23 @@
 import {AppNavigation} from '@/navigation/AppNavigation';
 import {persistor, store} from '@/store';
 import {NavigationContainer} from '@react-navigation/native';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import React from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
+import '@/configs/themes/unitstyles';
+
+const queryClient = new QueryClient();
 
 function App(): React.JSX.Element {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <NavigationContainer>
-          <AppNavigation />
-        </NavigationContainer>
+        <QueryClientProvider client={queryClient}>
+          <NavigationContainer>
+            <AppNavigation />
+          </NavigationContainer>
+        </QueryClientProvider>
       </PersistGate>
     </Provider>
   );
